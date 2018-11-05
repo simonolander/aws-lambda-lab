3c4128fe7b4cd890173ed15fa6695b3f1f92deca
Simon Olander <simon.olander@r2m.se>
2018-11-05 16:16:00 +0100

    Cleaned up the lambda functions a bit

--- lambdas/getMessages/lambda_function.py
+++ lambdas/getMessages/lambda_function.py
@@ -2,7 +2,6 @@ import sys
 import logging
 import rds_config
 import pymysql
-import json
 
 """ Fetching config information and credentials from rds_config.py """
 rds_host = rds_config.rds_host
@@ -16,37 +15,40 @@ logger.setLevel(logging.INFO)
 
 """
 Setting up database connection. 
-This is done outside the lambda handler because we don't need to initialize the connection every time we run the lambda
+This is done outside the lambda handler because we don't need to 
+initialize the connection every time we run the lambda
 """
 try:
-    conn = pymysql.connect(rds_host, user=rds_username, passwd=rds_password, db=rds_db_name, connect_timeout=5)
+    connection = pymysql.connect(rds_host, user=rds_username,
+                                 passwd=rds_password, db=rds_db_name, connect_timeout=5)
 except Exception as e:
     logger.error("Error connecting to database: " + str(e))
     sys.exit()
 
-logger.info("SUCCESS: Connection to database succeeded")
+logger.info("Connection to database succeeded")
 
-"""
-Validates the given event and extracts parameters.
 
-Expected event shape:
-{
-    "body-json" : {},
-    "params" : {
-    "path" : {},
-    "querystring" : {
-        "limit" : (string between 0 and 2**31-1, or nothing)
-    },
-    "header" : {}
-    },
-    "stage-variables" : {}
-}
-
-returns: {
-    "limit": int
-}
-"""
 def validate_event(event):
+    """
+    Validates the given event and extracts parameters.
+
+    Expected event shape:
+    {
+        "body-json" : {},
+        "params" : {
+        "path" : {},
+        "querystring" : {
+            "limit" : (string between 0 and 2**31-1, or nothing)
+        },
+        "header" : {}
+        },
+        "stage-variables" : {}
+    }
+
+    returns: {
+        "limit": int
+    }
+    """
     if type(event) is not dict:
         logger.info(event)
         raise Exception("Bad Request: event is not a dict")
@@ -69,7 +71,8 @@ def validate_event(event):
 
     if type(querystring) is not dict:
         logger.info(event)
-        raise Exception("Bad Request: event['params']['querystring'] is not a dict")
+        raise Exception(
+            "Bad Request: event['params']['querystring'] is not a dict")
 
     max_limit = 2147483647
     if "limit" in querystring:
@@ -77,33 +80,34 @@ def validate_event(event):
             limit = int(querystring["limit"])
             if limit < 0:
                 logger.info(event)
-                raise Exception("Bad Request: event['params']['querystring']['limit'] can't be negative but is " + querystring["limit"])
+                raise Exception(
+                    "Bad Request: event['params']['querystring']['limit'] can't be negative but is " + querystring["limit"])
             if limit > max_limit:
                 logger.info(event)
-                raise Exception("Bad Request: event['params']['querystring']['limit'] can't be greater than " 
+                raise Exception("Bad Request: event['params']['querystring']['limit'] can't be greater than "
                                 + str(max_limit) + " but is " + querystring["limit"])
         except ValueError:
-            raise Exception("Bad Request: event['params']['querystring']['limit'] '" + querystring["limit"] + "' is not a valid number")
+            raise Exception(
+                "Bad Request: event['params']['querystring']['limit'] '" + querystring["limit"] + "' is not a valid number")
     else:
         limit = max_limit
 
-    return {
-        "limit": limit
-    }
+    return limit
 
-"""
-This is the main method of the lambda function. The default AWS name is lambda_handler.
-The parameter event contains all the parameters from the rest request.
-The parameter context contains a lot of system properties that we don't care about.
 
-The method executes a select against the database connection and returns the result as a list of dicts.
-"""
 def lambda_handler(event, context):
-    parameters = validate_event(event)
-    with conn.cursor() as cur:
-        sql = "SELECT id, username, message, created_time FROM messages ORDER BY created_time DESC LIMIT %s"
-        cur.execute(sql, (parameters["limit"],))
-        conn.commit()
+    """
+    This is the main method of the lambda function. The default AWS name is lambda_handler.
+    The parameter event contains all the parameters from the rest request.
+    The parameter context contains a lot of system properties that we don't care about.
+
+    The method executes a select against the database connection and returns the result as a list of dicts.
+    """
+    limit = validate_event(event)
+    with connection.cursor() as cursor:
+        connection.commit()
+        statement = "SELECT id, username, message, created_time FROM messages ORDER BY created_time DESC LIMIT %s"
+        cursor.execute(statement, (limit,))
         return [
             {
                 "id": row[0],
@@ -111,5 +115,5 @@ def lambda_handler(event, context):
                 "message": row[2],
                 "created_time": str(row[3].astimezone()),
             }
-            for row in cur
+            for row in cursor
         ]
--- lambdas/postMessage/lambda_function.py
+++ lambdas/postMessage/lambda_function.py
@@ -14,41 +14,42 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 """
-Setting up database connection. 
+Setting up database connection.
 This is done outside the lambda handler because we don't need to initialize the connection every time we run the lambda
 """
 try:
-    conn = pymysql.connect(rds_host, user=rds_username, passwd=rds_password, db=rds_db_name, connect_timeout=5)
+    connection = pymysql.connect(rds_host, user=rds_username,
+                                 passwd=rds_password, db=rds_db_name, connect_timeout=5)
 except Exception as e:
     logger.error("Error connecting to database: " + str(e))
     sys.exit()
 
-logger.info("SUCCESS: Connection to database succeeded")
+logger.info("Connection to database succeeded")
 
 
-"""
-Validates the given event and extracts parameters.
-
-Expected event shape:
-{
-    "body-json" : {
-        "message": String (max 255 characters),
-        "username": String
-    },
-    "params" : {
-        "path" : {},
-        "querystring" : {},
-        "header" : {}
-    },
-    "stage-variables" : {}
-}
-
-returns: {
-    "username": string,
-    "message": string,
-}
-"""
 def validate_event(event):
+    """
+    Validates the given event and extracts parameters.
+
+    Expected event shape:
+    {
+        "body-json" : {
+            "message": String (max 255 characters),
+            "username": String
+        },
+        "params" : {
+            "path" : {},
+            "querystring" : {},
+            "header" : {}
+        },
+        "stage-variables" : {}
+    }
+
+    returns: {
+        "username": string,
+        "message": string,
+    }
+    """
     if type(event) is not dict:
         logger.info(event)
         raise Exception("Bad Request: event is not a dict")
@@ -71,10 +72,12 @@ def validate_event(event):
 
     if type(username) is not str:
         logger.info(event)
-        raise Exception("Bad Request: event['body-json']['username'] is not a str")
+        raise Exception(
+            "Bad Request: event['body-json']['username'] is not a str")
 
     if len(username) > 255:
-        raise Exception("Bad Request: event['body-json']['username'] can't be larger than 255 characters, but is " + username)
+        raise Exception(
+            "Bad Request: event['body-json']['username'] can't be larger than 255 characters, but is " + username)
 
     if "message" not in body:
         logger.info(event)
@@ -84,39 +87,37 @@ def validate_event(event):
 
     if type(message) is not str:
         logger.info(event)
-        raise Exception("Bad Request: event['body-json']['message'] is not a str")
+        raise Exception(
+            "Bad Request: event['body-json']['message'] is not a str")
 
-    return {
-        "username": username,
-        "message": message,
-    }
+    return username, message
 
 
-"""
-This is the main method of the lambda function. The default AWS name is lambda_handler.
-The parameter event contains all the parameters from the rest request.
-The parameter context contains a lot of system properties that we don't care about.
-
-The method executes a an insert into the database with the provided username and message and returns the result.
-
-The procedure looks like this:
-
-create procedure insert_message(
-    in p_username varchar(255),
-    in p_message text
-)
-begin
-insert into messages (username, message) values (p_username, p_message);
-select id, username, message, created_time from messages where id=last_insert_id();
-end
-
-"""
 def lambda_handler(event, context):
-    parameters = validate_event(event)
-    with conn.cursor() as cur:
-        sql = "CALL insert_message(%s, %s)"
-        result_code = cur.execute(sql, (parameters["username"], parameters["message"]))
-        result = cur.fetchall()
+    """
+    This is the main method of the lambda function. The default AWS name is lambda_handler.
+    The parameter event contains all the parameters from the rest request.
+    The parameter context contains a lot of system properties that we don't care about.
+
+    The method executes a an insert into the database with the provided username and message and returns the result.
+
+    The procedure looks like this:
+
+        create procedure insert_message(
+            in p_username varchar(255),
+            in p_message text
+        )
+        begin
+        insert into messages (username, message) values (p_username, p_message);
+        select id, username, message, created_time from messages where id=last_insert_id();
+        end
+
+    """
+    username, message = validate_event(event)
+    with connection.cursor() as cursor:
+        statement = "CALL insert_message(%s, %s)"
+        cursor.execute(statement, (username, message))
+        result = cursor.fetchall()
         logger.info("Inserted message: " + str(result))
         inserted_message = {
             "id": result[0][0],
@@ -124,5 +125,5 @@ def lambda_handler(event, context):
             "message": result[0][2],
             "created_time": str(result[0][3].astimezone()),
         }
-        conn.commit()
+        connection.commit()
         return inserted_message
